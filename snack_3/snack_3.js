d5b51521216d78aa664e527796b5cdf1ccf88bed
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-30 11:34:02 +0200

    add end snack_3

---
+++ snack_3/snack_3.js
@@ -0,0 +1,17 @@
+/* 
+SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*/
+
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+
+for (let i = 0; i < 10; i++){
+    const inserire_numero = parseInt(prompt('Inserisci un numero'));
+    console.log(inserire_numero);
+}
+
+//Il programma stampa la somma di tutti i numeri inseriti.
