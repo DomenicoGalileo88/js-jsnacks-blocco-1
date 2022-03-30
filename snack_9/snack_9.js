31620dc9b9e88c288f8c05162905e33898420d30
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-30 17:04:23 +0200

    Add end snack 9

---
+++ snack_9/snack_9.js
@@ -0,0 +1,16 @@
+// Calcola la somma e la media dei primi 10 numeri
+
+// ho 10 numeri
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// calcolo la loro somma e la loro media
+
+let somma = 0;
+let media;
+for (let i = 0; i < numeri.length; i++) {
+    const element = numeri[i];
+    somma = somma + element;
+    media = somma / numeri.length;
+}
+
+console.log(`La somma dei numeri è: ${somma} e la media è: ${media}`);
