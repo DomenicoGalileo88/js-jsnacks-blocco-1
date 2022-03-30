d84de9e9b6d520789f7b8fe293f14dfc36e7cb81
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-30 12:41:13 +0200

    Add snack_5

--- snack_3/snack_3.js
+++ snack_3/snack_3.js
@@ -8,7 +8,7 @@ Il programma stampa la somma di tutti i numeri inseriti.
 
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 
-let somma, inserire_numero:
+let somma, inserire_numero;
 somma = 0;
 
 for (let i = 0; i < 10; i++){
---
+++ snack_5/snack_5.js
@@ -0,0 +1,23 @@
+/* 
+SNACK 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+
+*/
+
+//Crea un array vuoto.
+
+let pari_dispari = [];
+
+//Chiedi per 6 volte all’utente di inserire un numero,
+
+for (let i = 0; i < 6; i++) {
+    let inserire_numero = parseInt(prompt('Inserire un numero'));
+    if ( inserire_numero % 2 == 0)
+    pari_dispari.push(inserire_numero)
+}
+
+console.log(pari_dispari);
+
+//se è dispari inseriscilo nell’array.
