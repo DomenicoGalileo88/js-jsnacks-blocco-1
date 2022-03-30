bd4db39e7b87a8983446fb8180e53bd4869e7ee2
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-30 15:50:44 +0200

    mod end snack_7

--- snack_7/snack_7.js
+++ snack_7/snack_7.js
@@ -2,7 +2,7 @@
 
 //crea un ciclo che stampi la pot di 2 fino a che il suo risultato sia 1000
 
-for (i = 1; i <= 10; i++){
+for (i = 1; i <= 1000; i++){
     let numero = Math.pow(2, i);
     console.log(numero);
 }
--- snack_8/snack_8.js
+++ snack_8/snack_8.js
@@ -1,24 +1,7 @@
 /* 
-SNACK 5
-Crea un array vuoto.
-Chiedi per 6 volte all’utente di inserire un numero,
-se è dispari inseriscilo nell’array.
-
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
 */
 
-//Crea un array vuoto.
 
-let pari_dispari = [];
-
-//Chiedi per 6 volte all’utente di inserire un numero,
-
-for (let i = 0; i < 6; i++) {
-    let inserire_numero = parseInt(prompt('Inserire un numero'));
-    
-    //se è dispari inseriscilo nell’array.
-    if ( inserire_numero % 2 !== 0)
-    pari_dispari.push(inserire_numero)
-}
-
-console.log(pari_dispari);
-
+//
